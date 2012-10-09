b94c6c063ef802abd42678b7a1725102762a5acb
Dolph Mathews <dolph.mathews@gmail.com>
2012-10-09 03:58:33 +0000

    Useful error msg when missing catalog (bug 949904)
    Change-Id: I498e9b79e9739437b7e61997b37e84283b496561

--- keystoneclient/v2_0/client.py
+++ keystoneclient/v2_0/client.py
@@ -76,10 +76,13 @@ class Client(client.HTTPClient):
         # extensions
         self.ec2 = ec2.CredentialsManager(self)
 
+        self.management_url = endpoint
         if endpoint is None:
             self.authenticate()
-        else:
-            self.management_url = endpoint
+
+    def has_service_catalog(self):
+        """Returns True if this client provides a service catalog."""
+        return hasattr(self, 'service_catalog')
 
     def authenticate(self):
         """ Authenticate against the Keystone API.
