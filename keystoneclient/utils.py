45ab2e8db759e626580b8702438b5e1f304dd193
Masayuki Igawa <igawa@mxs.nes.nec.co.jp>
2012-12-10 19:18:02 +0900

    Fix keystoneclient user-list output order
    keystone user-list output is ordered by 'id' now.
    But this order(by 'id') is not human-friendly. Sorting by 'name' is easy to
    identify a user.
    
    This patch fixes this problem.
    
    Fixes bug 1073437
    
    Change-Id: I00b1f46c248544157e09b2efccfff63a1f0426aa

--- keystoneclient/utils.py
+++ keystoneclient/utils.py
@@ -20,7 +20,7 @@ def pretty_choice_list(l):
     return ', '.join("'%s'" % i for i in l)
 
 
-def print_list(objs, fields, formatters={}):
+def print_list(objs, fields, formatters={}, order_by=None):
     pt = prettytable.PrettyTable([f for f in fields], caching=False)
     pt.aligns = ['l' for f in fields]
 
@@ -37,7 +37,9 @@ def print_list(objs, fields, formatters={}):
                 row.append(data)
         pt.add_row(row)
 
-    print pt.get_string(sortby=fields[0])
+    if order_by is None:
+        order_by = fields[0]
+    print pt.get_string(sortby=order_by)
 
 
 def _word_wrap(string, max_length=0):
