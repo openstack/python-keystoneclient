20bc75e6cc44c07abebf772f7e54b02f343d9508
Mark J. Washenberger <mark.washenberger@markwash.net>
2012-12-10 17:05:37 -0800

    Peg version to 0.2.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ tests_require = setup.parse_requirements(['tools/test-requires'])
 
 setuptools.setup(
     name="python-keystoneclient",
-    version=setup.get_post_version('keystoneclient'),
+    version='0.2.0',
     description="Client library for OpenStack Identity API (Keystone)",
     long_description=read('README.rst'),
     url='https://github.com/openstack/python-keystoneclient',
