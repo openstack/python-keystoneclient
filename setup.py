108b32da4217302eacb1eb0e20160b44572020ed
Monty Taylor <mordred@inaugust.com>
2013-05-10 18:15:49 -0400

    Migrate to pbr.
    Fixes bug 1179007
    
    Change-Id: Iceb63c223faab49be01baa36c035f703ddf59fc0

--- setup.py
+++ setup.py
@@ -1,44 +1,21 @@
-import os
-import sys
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import setuptools
 
-from keystoneclient.openstack.common import setup
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-requires = setup.parse_requirements()
-depend_links = setup.parse_dependency_links()
-tests_require = setup.parse_requirements(['tools/test-requires'])
-
 setuptools.setup(
-    name="python-keystoneclient",
-    version=setup.get_version('python-keystoneclient'),
-    description="Client library for OpenStack Identity API (Keystone)",
-    long_description=read('README.rst'),
-    url='https://github.com/openstack/python-keystoneclient',
-    license='Apache',
-    author='Nebula Inc, based on work by Rackspace and Jacob Kaplan-Moss',
-    author_email='gabriel.hurley@nebula.com',
-    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: OpenStack',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
-    install_requires=requires,
-    dependency_links=depend_links,
-    cmdclass=setup.get_cmdclass(),
-
-    tests_require=tests_require,
-
-    entry_points={
-        'console_scripts': ['keystone = keystoneclient.shell:main']
-    },
-)
+    setup_requires=['d2to1>=0.2.10,<0.3', 'pbr>=0.5,<0.6'],
+    d2to1=True)
