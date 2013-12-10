045e03d85284f4b0f5fffc656d90b922fb348b42
Steven Hardy <shardy@redhat.com>
2013-12-10 08:51:52 +0000

    Fix error in v3 credentials create/update
    The v3 credentials passes data associated with the credential in
    a "data" key, but the underlying API expects "blob", which is also
    what is documented in the API spec.
    
    Currently any attempt to create a credential with a type of 'ec2'
    will fail with 'Invalid blob in credential' So s/data/blob to fix.
    
    Change-Id: I0858e8c39653477eb554ee9d15fb4f2dde2b195c
    Closes-Bug: #1259461

--- keystoneclient/v3/credentials.py
+++ keystoneclient/v3/credentials.py
@@ -33,11 +33,25 @@ class CredentialManager(base.CrudManager):
     collection_key = 'credentials'
     key = 'credential'
 
-    def create(self, user, type, data, project=None):
+    def _get_data_blob(self, blob, data):
+        # Ref bug #1259461, the <= 0.4.1 keystoneclient calling convention was
+        # to pass "data", but the underlying API expects "blob", so
+        # support both in the python API for backwards compatibility
+        if blob is not None:
+            return blob
+        elif data is not None:
+            # FIXME(shardy): Passing data is deprecated. Provide an
+            # appropriate warning.
+            return data
+        else:
+            raise ValueError(
+                "Credential requires blob to be specified")
+
+    def create(self, user, type, blob=None, data=None, project=None):
         return super(CredentialManager, self).create(
             user_id=base.getid(user),
             type=type,
-            data=data,
+            blob=self._get_data_blob(blob, data),
             project_id=base.getid(project))
 
     def get(self, credential):
@@ -47,12 +61,13 @@ class CredentialManager(base.CrudManager):
     def list(self):
         return super(CredentialManager, self).list()
 
-    def update(self, credential, user, type=None, data=None, project=None):
+    def update(self, credential, user, type=None, blob=None, data=None,
+               project=None):
         return super(CredentialManager, self).update(
             credential_id=base.getid(credential),
             user_id=base.getid(user),
             type=type,
-            data=data,
+            blob=self._get_data_blob(blob, data),
             project_id=base.getid(project))
 
     def delete(self, credential):
