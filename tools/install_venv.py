7a71e1b90ebf1c58fb47d823aa9a7be2406eae17
Qiu Yu <qiuyu@ebaysf.com>
2014-01-06 16:35:10 +0800

    Use install_venv from oslo to fix no post_process issue
    In a recent oslo sync commit Ieb73d5f799423fa8abf71634c86601ba6d32df2f
    post_process() has been removed from tools/install_venv_common.py, which
    is still called in tools/install_venv.py
    
    Further investigation shows install_venv.py has some legacy distro
    specific code, which should be removed at the time introducing
    install_venv_common from oslo.
    
    This change switch tools/install_venv.py to use oslo code to align the
    effort as well as fixing original no 'post_process' issue.
    
    Changes being pulled into in install_venv are:
    12bcdb7 - 2013-11-25 12:16:11 +0800 - Remove vim header
    b5cca99 - 2013-10-09 21:34:00 +0100 - Remove obsolete
    redhat-eventlet.patch
    9378d3c - 2013-08-13 18:06:11 +0300 - Move `./run_tests.sh` to Oslo
    
    Note: openstack-common.conf is not updated, because the project name
    variable in install_venv has to be modified after syncing from oslo.
    That makes it not an exact sync but modification after sync. Besides
    other openstack projects using install_venv from oslo also don't change
    openstack-common.conf. So this patch simply follow the common practice
    here.
    
    Change-Id: I78a7563fbbde70cfd98cf1bd7a5a4eec5370d31d
    Closes-Bug: #1263940

--- tools/install_venv.py
+++ tools/install_venv.py
@@ -1,10 +1,8 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
 # Copyright 2010 United States Government as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
 #
-# Copyright 2010 OpenStack, LLC
+# Copyright 2010 OpenStack Foundation
 # Copyright 2013 IBM Corp.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -19,107 +17,55 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-"""
-Installation script for python-keystoneclient's development virtualenv
-"""
-
 import os
 import sys
 
-import install_venv_common as install_venv
+import install_venv_common as install_venv  # noqa
 
 
-class Debian(install_venv.Distro):
-    """This covers all Debian-based distributions."""
-
-    def check_pkg(self, pkg):
-        return run_command_with_code(['dpkg', '-l', pkg],
-                                     check_exit_code=False)[1] == 0
-
-    def apt_install(self, pkg, **kwargs):
-        run_command(['sudo', 'apt-get', 'install', '-y', pkg], **kwargs)
-
-    def apply_patch(self, originalfile, patchfile):
-        run_command(['patch', originalfile, patchfile])
-
-    def install_virtualenv(self):
-        if self.check_cmd('virtualenv'):
-            return
-
-        if not self.check_pkg('python-virtualenv'):
-            self.apt_install('python-virtualenv', check_exit_code=False)
-
-        super(Debian, self).install_virtualenv()
-
-
-class Suse(install_venv.Distro):
-    """This covers all SuSE distributions."""
-
-    def check_pkg(self, pkg):
-        return run_command_with_code(['rpm', '-q', pkg],
-                                     check_exit_code=False)[1] == 0
-
-    def zypper_install(self, pkg, **kwargs):
-        run_command(['sudo', 'zypper', '-qn', 'install', pkg], **kwargs)
-
-    def apply_patch(self, originalfile, patchfile):
-        run_command(['patch', originalfile, patchfile])
-
-    def install_virtualenv(self):
-        if self.check_cmd('virtualenv'):
-            return
-
-        if not self.check_pkg('python-virtualenv'):
-            self.zypper_install('python-virtualenv', check_exit_code=False)
-
-        super(Suse, self).install_virtualenv()
-
-
-def print_help():
+def print_help(venv, root):
     help = """
-    python-keystoneclient development environment setup is complete.
+    Openstack development environment setup is complete.
 
-    python-keystoneclient development uses virtualenv to track and manage
-    Python dependencies while in development and testing.
+    Openstack development uses virtualenv to track and manage Python
+    dependencies while in development and testing.
 
-    To activate the python-keystoneclient virtualenv for the extent of your
-    current shell session you can run:
+    To activate the Openstack virtualenv for the extent of your current shell
+    session you can run:
 
-    $ source .venv/bin/activate
+    $ source %s/bin/activate
 
     Or, if you prefer, you can run commands in the virtualenv on a case by case
     basis by running:
 
-    $ tools/with_venv.sh <your command>
+    $ %s/tools/with_venv.sh <your command>
 
     Also, make test will automatically use the virtualenv.
     """
-    print(help)
+    print(help % (venv, root))
 
 
 def main(argv):
     root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+    if os.environ.get('tools_path'):
+        root = os.environ['tools_path']
     venv = os.path.join(root, '.venv')
+    if os.environ.get('venv'):
+        venv = os.environ['venv']
+
     pip_requires = os.path.join(root, 'requirements.txt')
     test_requires = os.path.join(root, 'test-requirements.txt')
     py_version = "python%s.%s" % (sys.version_info[0], sys.version_info[1])
     project = 'python-keystoneclient'
     install = install_venv.InstallVenv(root, venv, pip_requires, test_requires,
                                        py_version, project)
-    if os.path.exists('/etc/SuSE-release'):
-        install_venv.Distro = Suse
-    elif os.path.exists('/etc/debian_version'):
-        install_venv.Distro = Debian
     options = install.parse_args(argv)
     install.check_python_version()
     install.check_dependencies()
     install.create_virtualenv(no_site_packages=options.no_site_packages)
     install.install_dependencies()
-    install.run_command([os.path.join(venv, 'bin/python'),
-                        'setup.py', 'develop'])
-    install.post_process()
-    print_help()
-
+    print_help(venv, root)
 
 if __name__ == '__main__':
     main(sys.argv)
