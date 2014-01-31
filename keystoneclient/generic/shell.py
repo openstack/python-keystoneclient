017bb0de3a6cac574612fe67876c5768f5b1adf0
Andreas Jaeger <aj@suse.de>
2014-01-31 19:22:26 +0100

    Improve output of "keystone help discover"
    The current output of "keystone help discover" is:
    
    usage: keystone discover
    
    Discover Keystone servers, supported API versions and extensions.  Usage:: $
    keystone discover Keystone found at http://localhost:35357 - supports version
    v1.0 (DEPRECATED) here http://localhost:35357/v1.0 - supports version v1.1
    (CURRENT) here http://localhost:35357/v1.1 - supports version v2.0 (CURRENT)
    here http://localhost:35357/v2.0 - and RAX-KSKEY: Rackspace API Key
    Authentication Admin Extension - and RAX-KSGRP: Rackspace Keystone Group
    Extensions
    
    The reason is that our parser strips away all the line ends. Let's
    rephrase the text so that it formats more nicely.
    
    Now the output is:
    Discover Keystone servers, supported API versions and extensions.
    
    Change-Id: Ic9f9e3e407ae299eb091a0a7093c955dc6cab8f2

--- keystoneclient/generic/shell.py
+++ keystoneclient/generic/shell.py
@@ -27,17 +27,6 @@ CLIENT_CLASS = client.Client
 @utils.unauthenticated
 def do_discover(cs, args):
     """Discover Keystone servers, supported API versions and extensions.
-
-    Usage::
-
-        $ keystone discover
-        Keystone found at http://localhost:35357
-
-    - supports version v1.0 (DEPRECATED) here http://localhost:35357/v1.0
-    - supports version v1.1 (CURRENT) here http://localhost:35357/v1.1
-    - supports version v2.0 (CURRENT) here http://localhost:35357/v2.0
-        - and RAX-KSKEY: Rackspace API Key Authentication Admin Extension
-        - and RAX-KSGRP: Rackspace Keystone Group Extensions
     """
     if cs.endpoint:
         versions = cs.discover(cs.endpoint)
