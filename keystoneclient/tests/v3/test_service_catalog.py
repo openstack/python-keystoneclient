e8f2631b91700d1776520c025ccd127998cc407b
zhang-jinnan <ben.os@99cloud.net>
2014-02-17 20:00:17 +0800

    Replace assertEqual(None, *) with assertIsNone in tests
    Replace assertEqual(None, *) with assertIsNone in tests to have
    more clear messages in case of failure.
    
    Change-Id: I8ba72d345ad00f5cde8696ab82c31606ca82a13f

--- keystoneclient/tests/v3/test_service_catalog.py
+++ keystoneclient/tests/v3/test_service_catalog.py
@@ -151,7 +151,7 @@ class ServiceCatalogTest(utils.TestCase):
         self.assertEqual(urls[0], self.south_endpoints['public'])
 
         urls = sc.get_urls(service_type='image', region_name='West')
-        self.assertEqual(urls, None)
+        self.assertIsNone(urls)
 
     def test_service_catalog_param_overrides_body_region(self):
         self.AUTH_RESPONSE_BODY['token']['region_name'] = "North"
