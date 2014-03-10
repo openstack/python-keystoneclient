84b82436fd710bd40a5b7251528430330771e7b1
Jamie Lennox <jamielennox@redhat.com>
2014-03-11 08:13:23 +1000

    Handle Token/Endpoint authentication
    This is essentially the Admin token where your authentication will
    always use a given token and have no means of querying information from
    within that token blob.
    
    Change-Id: Ia4317836d6fc1e62c65a1dcbd84c820d90832dc4

---
+++ keystoneclient/tests/auth/test_token_endpoint.py
@@ -0,0 +1,35 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import httpretty
+
+from keystoneclient.auth import token_endpoint
+from keystoneclient import session
+from keystoneclient.tests import utils
+
+
+class TokenEndpointTest(utils.TestCase):
+
+    TEST_TOKEN = 'aToken'
+    TEST_URL = 'http://server/prefix'
+
+    @httpretty.activate
+    def test_basic_case(self):
+        httpretty.register_uri(httpretty.GET, self.TEST_URL, body='body')
+
+        a = token_endpoint.Token(self.TEST_URL, self.TEST_TOKEN)
+        s = session.Session(auth=a)
+
+        data = s.get(self.TEST_URL, authenticated=True)
+
+        self.assertEqual(data.text, 'body')
+        self.assertRequestHeaderEqual('X-Auth-Token', self.TEST_TOKEN)
