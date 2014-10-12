3568acb61732d1ce090b41e2c991307a257070db
Brant Knudson <bknudson@us.ibm.com>
2014-10-12 18:43:01 -0500

    Docstring usability improvements
    The generated docs didn't provide enough information for a developer
    to get started using the API. This change enhances the documentation
    for the module so that a developer knows where to go to start (create
    a Client).
    
    Partial-Bug: #1330769
    Change-Id: I907187d34ebf2c2e662ff7b9547b0ecaef008414

--- keystoneclient/__init__.py
+++ keystoneclient/__init__.py
@@ -15,9 +15,15 @@
 
 """The python bindings for the OpenStack Identity (Keystone) project.
 
-See :py:class:`keystoneclient.v3.client.Client` for the Identity V3 client.
+A Client object will allow you to communicate with the Identity server. The
+recommended way to get a Client object is to use
+:py:func:`keystoneclient.client.Client()`. :py:func:`~.Client()` uses version
+discovery to create a V3 or V2 client depending on what versions the Identity
+server supports and what version is requested.
 
-See :py:class:`keystoneclient.v2_0.client.Client` for the Identity V2.0 client.
+Identity V2 and V3 clients can also be created directly. See
+:py:class:`keystoneclient.v3.client.Client` for the V3 client and
+:py:class:`keystoneclient.v2_0.client.Client` for the V2 client.
 
 """
 
