641aa0f91e34f19c1cc7880565eb3a3f0da4876e
Ivan Udovichenko <iudovichenko@mirantis.com>
2016-04-27 16:34:48 +0300

    Fallback if Git repository is absent
    Documentation build fails during packaging if Git repository is absent.
    We do not package .git directory and that is why it leads to fails during
    documentation build.
    With this change we are certain that it will not fail.
    
    This change was originally proposed by Davanum Srinivas (dims):
    https://review.openstack.org/287448/
    
    Change-Id: I49dce2537ea26c168af9a67d398930042702762c

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -150,9 +150,13 @@ modindex_common_prefix = ['keystoneclient.']
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 git_cmd = ["git", "log", "--pretty=format:'%ad, commit %h'", "--date=local",
-   "-n1"]
-html_last_updated_fmt = subprocess.Popen(git_cmd,
+           "-n1"]
+try:
+    html_last_updated_fmt = subprocess.Popen(git_cmd,
                                      stdout=subprocess.PIPE).communicate()[0]
+except Exception:
+    warnings.warn('Cannot get last updated time from git repository. '
+                  'Not setting "html_last_updated_fmt".')
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
