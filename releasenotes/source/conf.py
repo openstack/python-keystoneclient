f7e75f43d811c8e1028746b2574322afa53dbaac
pengyuesheng <pengyuesheng@gohighsec.com>
2019-07-03 14:49:58 +0800

    Bump the openstackdocstheme extension to 1.20
    Some options are now automatically configured by the version 1.20:
    - project
    - html_last_updated_fmt
    - latex_engine
    - latex_elements
    - version
    - release.
    
    Depends-On:https://review.opendev.org/#/c/660609/
    
    Change-Id: I83ee2a89ae0a8158ed955581b738cea6ca93707d

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -54,7 +54,6 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'keystoneclient Release Notes'
 copyright = u'2015, Keystone Developers'
 
 # Release notes are version independent.
@@ -142,11 +141,6 @@ html_static_path = ['_static']
 # directly to the root of the documentation.
 # html_extra_path = []
 
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
-html_last_updated_fmt = '%Y-%m-%d %H:%M'
-
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
 # html_use_smartypants = True
@@ -189,17 +183,6 @@ htmlhelp_basename = 'KeystoneClientReleaseNotesdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    # 'preamble': '',
-}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
